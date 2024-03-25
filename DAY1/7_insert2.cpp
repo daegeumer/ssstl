d49f284fb36b322b9a471f078cc8dbb47f07ce72
ecourse <smkang@codenuri.co.kr>
2024-03-25 15:51:38 +0900

    Add files via upload

---
+++ DAY1/7_insert2.cpp
@@ -0,0 +1,17 @@
+﻿// insert2.cpp
+#include <iostream>
+#include <algorithm>
+#include <list>
+
+
+int main(int argc, char** argv)
+{	
+	std::list<int> s1 = { 1, 2, 3, 4, 5 };
+	std::list<int> s2 = { 0, 0, 0, 0, 0 };
+	
+	std::copy(s1.begin(), s1.end(), s2.begin()); 
+
+
+	for (auto& n : s2)
+		std::cout << n << std::endl; 
+}
