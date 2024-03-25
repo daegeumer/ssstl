cf08204e7bee4a252da6c5021e601a53a6822a32
ecourse <smkang@codenuri.co.kr>
2024-03-25 13:38:46 +0900

    Add files via upload

---
+++ DAY1/5_iterator4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <list>   
+#include <vector>
+
+// 반복자 무효화
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5 };
+	
+	auto p = v.begin();
+
+	std::cout << *p << std::endl; // 1
+
+//	v.resize(10);   // 이순간 v가 사용하는 내부 버퍼가 변경됩니다.
+					// 이전에 꺼낸 모든 반복자는 사용하면 안됩니다.
+					// "iterator invalidate" 라고 표현
+	v.resize(3);    // 작아지면 버퍼 변경 안됩니다. 
+					// 반복자 무효화 안됨
+
+	std::cout << *p << std::endl; // ?
+}
+
+
+
