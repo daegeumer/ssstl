f44e662c08724ca46837aaf1efea3599ed68769d
ecourse <smkang@codenuri.co.kr>
2024-03-26 08:54:33 +0900

    Add files via upload

--- DAY2/1_stream5.cpp
+++ DAY2/1_stream5.cpp
@@ -7,11 +7,17 @@
 int main()
 {
 	std::ifstream fin("1_stream5.cpp"); // 현재 파일 이름
+	// 아래 반복자는 white space(공백, tab, backspace 등) 입력 안됩니다.
+//	std::istream_iterator<char> p1(fin);
+//	std::istream_iterator<char> p2;		// EOF
 
-	std::istream_iterator<char> p1(fin);
-	std::istream_iterator<char> p2;		
+	// 아래 반복자는 white space 입니다.
+	std::istreambuf_iterator<char> p1(fin);
+	std::istreambuf_iterator<char> p2;		// EOF
+
 
 	std::ostream_iterator<char> p3(std::cout, "");		
 
 	std::copy(p1, p2, p3);
 }
+// 터미널에서  "chcp 65001" 입력하면 한글 제대로 출력됩니다.
