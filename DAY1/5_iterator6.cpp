759eb0e1a78ff7b70b2f172129dbc245441a2937
ecourse <smkang@codenuri.co.kr>
2024-03-25 13:46:12 +0900

    Add files via upload

--- DAY1/5_iterator6.cpp
+++ DAY1/5_iterator6.cpp
@@ -11,7 +11,18 @@ int main()
 	for(int i = 0; i < 5; i++)
 		x[i] = y[i];
 
-	std::list<int> s2 = { 0,0,0,0,0 };
+	std::list<int> s = { 0,0,0,0,0 };
 
 	// #2. y의 모든 요소를 s 에 복사
+//	for(int i = 0; i < 5; i++)
+//		s[i] = y[i];	// error. list 는 [] 연산 안됩니다.
+
+	// 반복자 사용
+	auto first = s.begin();
+	auto last = s.end();
+	int i = 0;
+	while( first != last )
+	{
+		*first = y[i++];
+	}
 }
