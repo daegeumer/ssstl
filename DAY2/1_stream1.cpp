e30232ced353c21855d341fb365b35a17b72a539
ecourse <smkang@codenuri.co.kr>
2024-03-26 08:40:22 +0900

    Add files via upload

--- DAY2/1_stream1.cpp
+++ DAY2/1_stream1.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <iterator>
 #include <list>
+#include <fstream> // C++에서 파일 입출력 객체
 
 int main()
 {
@@ -11,9 +12,14 @@ int main()
 	// 출력 스트림 반복자
 	std::ostream_iterator<int> p(std::cout, " ");
 
-	*p = 10;
-	*p = 20;
+	*p = 10; // std::cout << 10 << " "
+	*p = 20; // std::cout << 10 << " "
 
 
+	std::ofstream fout("a.txt"); // 출력 파일 객체
+	std::ostream_iterator<int> p2(fout, " ");
+
+	*p2 = 10; // a.txt 에 출력
+	*p2 = 20;
 
 }
