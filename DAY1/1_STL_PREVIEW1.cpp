67e4ca5d1610c02687661392b930ed0c45a3dbee
ecourse <smkang@codenuri.co.kr>
2024-03-25 09:37:06 +0900

    Add files via upload

---
+++ DAY1/1_STL_PREVIEW1.cpp
@@ -0,0 +1,14 @@
+﻿// 1_STL_PREVIEW1
+#include <iostream>
+#include <list>
+#include <vector>
+int main()
+{
+	int x[5] = {1,2,3,4,5};
+
+	// 배열 : 크기 변경을 할수 없습니다
+	// vector : 크기 변경이 가능합니다.
+	std::vector<int> v = {1,2,3,4,5};
+
+	v.resize(10); // 10개로 크기 변경
+}
---
+++ DAY1/1_STL_PREVIEW2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+
+int main()
+{
+	// 컨테이너(container) : 여러개의 요소를 보관하는 자료구조
+	std::vector<int> v = {1,2,3,4,5}; // 모든 요소가 연속된 메모리에
+	std::list<int> s = {1,2,3,4,5}; // 모든 요소가 떨어진 메모리에
+
+	// C++17 부터 타입생략가능합니다.
+	std::vector v2 = {1,2,3,4,5};
+	std::vector v3{1,2,3,4,5};
+}
