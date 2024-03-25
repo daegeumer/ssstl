f3d0a3fbd52532421c6f56384cacc140a5a4a502
ecourse <smkang@codenuri.co.kr>
2024-03-25 15:54:31 +0900

    Add files via upload

--- DAY1/7_insert2.cpp
+++ DAY1/7_insert2.cpp
@@ -9,7 +9,19 @@ int main(int argc, char** argv)
 	std::list<int> s1 = { 1, 2, 3, 4, 5 };
 	std::list<int> s2 = { 0, 0, 0, 0, 0 };
 	
-	std::copy(s1.begin(), s1.end(), s2.begin()); 
+	// #1. s1 의 모든 요소를 s2에 "덮어쓴것"
+//	std::copy(s1.begin(), s1.end(), s2.begin()); 
+
+	// #2. s1의 모든 요소를 s2에 "추가" - push_back 사용
+//	for( auto& e : s1)
+//		s2.push_back(e);
+
+	// #3. 삽입반복자 - 클래스 버전 사용
+	std::back_insert_iterator< std::list<int>> p(s2);
+
+	std::copy( s1.begin(), s1.end(), p);
+
+
 
 
 	for (auto& n : s2)
