44ebb7c9c4061a6c06f7d3b8204f12ec864b5f31
ecourse <smkang@codenuri.co.kr>
2024-03-26 08:35:47 +0900

    Add files via upload

---
+++ DAY2/1_stream1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <iterator>
+#include <list>
+
+int main()
+{
+	int n = 10;
+
+	std::cout << n << std::endl;
+
+	// 출력 스트림 반복자
+	std::ostream_iterator<int> p(std::cout, " ");
+
+	*p = 10;
+	*p = 20;
+
+
+
+}
