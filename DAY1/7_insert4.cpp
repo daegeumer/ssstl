0bb566afdd8c8c76ca87d6cec4696937642bc8e8
ecourse <smkang@codenuri.co.kr>
2024-03-25 16:18:31 +0900

    Add files via upload

--- DAY1/7_insert4.cpp
+++ DAY1/7_insert4.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <list>
 
+// 삽입반복자의 원리
+template<typename T> 
+class my_back_insert_iterator
+{
+	T* cont;
+public:
+	my_back_insert_iterator(T& c) : cont(&c) {}
+
+	my_back_insert_iterator& operator*() { return *this;}
+
+	my_back_insert_iterator& operator= ( const typename T::value_type& value )
+	{
+		cont->push_back(value);
+		return *this;
+	}
+};
+
 int main()
 {
 	std::list<int> s = {1,2,3};
 
 	my_back_insert_iterator< std::list<int> > p(s);
 
-	*p = 10;
+	*p = 10; // s.push_back(10)
+//  (p.operator*()) = 10;
 }
