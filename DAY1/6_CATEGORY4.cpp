2996da5fce36af475f33797e6113bc8e2f4ae960
ecourse <smkang@codenuri.co.kr>
2024-03-25 15:07:29 +0900

    Add files via upload

--- DAY1/6_CATEGORY4.cpp
+++ DAY1/6_CATEGORY4.cpp
@@ -1,15 +1,18 @@
 // ALGORITHM5.cpp  복사해오세요 - 주석 제거
 #include <iostream>
+// template 인자의 이름을 통해서
+// 알고리즘이 요구하는 "반복자 category" 를 알려주는 것이 관례 입니다.
 
-template<typename T1, typename T2>
-T1 find(T1 first, T1 last, T2 c)
+// cppreference.com 에서 std::find 검색
+
+template<typename InputIter, typename T>
+InputIter find(InputIter first, InputIter last, const T& c)
 {
 	while ( first != last && *first != c)
 		++first;
 
 	return first;
 }
-
 int main()
 {
 	double x[10] = {1,2,3,4,5,6,7,8,9,10};
