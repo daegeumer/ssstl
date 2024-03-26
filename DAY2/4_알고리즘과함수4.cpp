213a1c8b14d85c53004c541ea1e7071ca5bee7e4
ecourse <smkang@codenuri.co.kr>
2024-03-26 10:47:24 +0900

    Add files via upload

---
+++ DAY2/4_알고리즘과함수4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "show.h"
+
+void foo(int n) { std::cout << n << ", ";}
+
+int main()
+{
+    std::vector<int> v = {1,2,3,4,5,6,7,8,9,10};
+	
+
+    std::for_each(v.begin(), v.end(), foo);
+
+	// C++11 부터 제공되는 람다 표현식 사용
+	// 람다표현식 : 함수 이름(주소)가 필요한 위치에 함수 구현 자체를 
+	//				적을수 있는 문법
+	// 				이름 없는 함수를 만드는 문법
+	// [] : lambda introducer
+	//      람다표현식이 시작됨을 알리는 기호
+
+	std::for_each(v.begin(), v.end(), 
+						[](int n) { std::cout << n << ", ";}  );
+
+}
+
+
