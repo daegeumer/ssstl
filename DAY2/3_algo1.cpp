905e3d95e5ce1b0fef7d0f21a94d560227f8f637
ecourse <smkang@codenuri.co.kr>
2024-03-26 09:48:53 +0900

    Add files via upload

---
+++ DAY2/3_algo1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+// 알고리즘 용어
+
+// 일반적인 의미 : 문제를 해결하는 방법
+// STL 에서는   : std::find 같은 일반함수 템플릿
+
+// <algorithm>, <numeric>
+
+// 핵심 : 멤버 함수가 아닌 일반 함수(템플릿)
+int main()
+{
+	std::vector<int> v = { 1,2,3,1,2,3,1,2,3,1 };
+	
+	auto p = std::find( v.begin(), v.end(), 3);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
