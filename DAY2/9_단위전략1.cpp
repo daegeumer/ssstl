dd620b7fa8b52408c6a5083dfc9332ab95b2af26
ecourse <smkang@codenuri.co.kr>
2024-03-26 15:34:21 +0900

    Add files via upload

---
+++ DAY2/9_단위전략1.cpp
@@ -0,0 +1,19 @@
+﻿template<typename T> class List
+{	
+public:
+	void push_front(const T& a)
+	{
+		// lock()
+		// ..... 
+		// unlock()
+	}
+};
+
+List<int> st;	// st는 전역변수. 모든 스레드가 공유. 
+				// 멀티스레드환경에서 안전하지 않다.
+int main()
+{
+	// lock()
+	st.push_front(10);
+	// unlock()
+}
