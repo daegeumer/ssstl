ac1d03c4b80663cb04af6888b87a27bcc1c3408a
ecourse <smkang@codenuri.co.kr>
2024-03-26 16:53:09 +0900

    Add files via upload

---
+++ DAY2/10_선형컨테이너3-2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include "show.h"
+
+
+int main()
+{
+	std::vector<int> v1;
+	std::vector<int> v2(1000);
+	
+	std::vector<int> v3;
+	v3.reserve(1000);
+
+	v1.push_back(0);
+	v2.push_back(0);
+	v3.push_back(0);
+
+
+
+//	std::cout << v.size() << ", " << v.capacity() << std::endl; // 10, 10
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
