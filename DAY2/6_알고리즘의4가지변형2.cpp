43ec56d699db04b70d31818a49091519ee49372b
ecourse <smkang@codenuri.co.kr>
2024-03-26 14:59:14 +0900

    Add files via upload

--- DAY2/6_알고리즘의4가지변형2.cpp
+++ DAY2/6_알고리즘의4가지변형2.cpp
@@ -11,9 +11,13 @@ int main()
 	// v1에서 3을 0으로 변경하는 코드입니다
 //	std::replace(v1.begin(), v1.end(), 3, 0);
 
-	// 위 코드 참고해서 v1에서 짝수를 제거한 결과를 v2 에 담아 보세요
+	// 위 코드 참고해서 v1에서 짝수를 0으로 변경한 결과를 v2 에 담아 보세요
 
+	std::replace_copy_if(v1.begin(), v1.end(), 
+						std::back_inserter(v2), 
+						 [](int n) { return n % 2 == 0;}, 0);
 
-	show(v2); // 1, 3, 5, 7, 9, 나와야 합니다
-
+	show(v2); // 1, 0, 3, 0, 5, 0, 7, 8, 9, 0  나와야 합니다
 }
+
+// cppreference.com 에서 1번째 화면. 오른쪽 위쪽에 "Algorithm library"
