b907e92794777bc876cbf4091067991207332a83
ecourse <smkang@codenuri.co.kr>
2024-03-26 12:08:57 +0900

    Add files via upload

--- DAY2/5_함수객체6.cpp
+++ DAY2/5_함수객체6.cpp
@@ -18,13 +18,30 @@ struct Greater
 {
 	inline bool operator()(int a, int b) { return a > b; }
 };
+//-------------------------------------------------------
+
+// 정책 교체가 가능한데, 정책의 인라인 치환을 지원하는 sort
+// => template 과 함수객체를 사용하는 기술
+// => STL 의 설계 철학!!(C++98 시절 부터 지원)
+template<typename T>
+void Sort(int* x, int sz, T cmp )
+{
+	for (int i = 0; i < sz - 1; i++)
+	{
+		for (int j = i + 1; j < sz; j++)
+		{
+			if( cmp(x[i], x[j]) == true )	
+				std::swap(x[i], x[j]);
+		}
+	}
+}
 
 int main()
 {
 	int x[10] = { 1,3,5,7,9,2,4,6,8,10 };
 
-	Less    f1; f1(1, 2); 
-	Greater f2; f2(1, 2); 
+	Less    f1; f1(1, 2); Sort(x, 10, f1); // ok
+	Greater f2; f2(1, 2); Sort(x, 10, f2); //  ?
 }
 
 
