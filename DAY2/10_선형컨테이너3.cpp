ae905cd84b50bf68c8a19875073f9e4da9f5da57
ecourse <smkang@codenuri.co.kr>
2024-03-26 16:44:14 +0900

    Add files via upload

--- DAY2/10_선형컨테이너3.cpp
+++ DAY2/10_선형컨테이너3.cpp
@@ -13,7 +13,22 @@ int main()
 
 	std::cout << v.size() << ", " << v.capacity() << std::endl; // 7, 10
 
-	
+	v.push_back(0); // 끝에 0 추가
+					// size < capacity 이므로 메모리 재할당 없음.
+					// not expensive!!
+
+	std::cout << v.size() << ", " << v.capacity() << std::endl; // 8, 10
+
+	v.shrink_to_fit();  // 필요없는 여분의 메모리 제거
+						// 8개 메모리 재할당해서, 기존 버퍼에서 8개 복사
+						// 기존 버퍼는 제거
+
+	std::cout << v.size() << ", " << v.capacity() << std::endl; // 8, 8
+
+	v.push_back(0);  // size == capacity 상태이므로 메모리 재할당 필요
+					// expensive 연산!!
+
+	std::cout << v.size() << ", " << v.capacity() << std::endl;	// 9, ?				
 }
 
 
