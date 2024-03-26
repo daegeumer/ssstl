0a659178e0f456d5bbd5a3945115294d7fba96c6
ecourse <smkang@codenuri.co.kr>
2024-03-26 16:13:01 +0900

    Add files via upload

--- DAY2/9_단위전략2-3.cpp
+++ DAY2/9_단위전략2-3.cpp
@@ -31,15 +31,22 @@ int main()
 	std::vector<int, debug_alloc<int> > v;
 
 	std::cout << "-----------------\n";
-	v.resize(5);
+	v.resize(5);	// 5개 할당
 
 	std::cout << "-----------------\n";
 	
-	v.resize(10);
+	v.resize(10);	// 1. 10개할당
+					// 2. 5개 => 10개 버퍼에 복사
+					// 3. 5개 해지
+
 	std::cout << "-----------------\n";
 	
-	v.resize(7);
+	v.resize(7);	// 줄어드는 경우는 메모리 재할당 없음
 	std::cout << "-----------------\n";
-}
+
+	v.clear();    // 메모리 제거안됨.
+	std::cout << "-----------------\n";
+
+}	// <= vector v 소멸자 호출   10개 제거.
 
 
