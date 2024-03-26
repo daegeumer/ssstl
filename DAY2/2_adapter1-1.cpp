40d359bba9fe05b1557fc687ef39567e834cefce
ecourse <smkang@codenuri.co.kr>
2024-03-26 09:38:06 +0900

    Add files via upload

--- DAY2/2_adapter1-1.cpp
+++ DAY2/2_adapter1-1.cpp
@@ -6,8 +6,14 @@ int main()
 {
 	std::list<int> s = { 1,2,3,4,5,6,7,3,9,10};
 
-	auto p1 = s.begin();
-	auto p2 = s.end();
+//	auto p1 = s.begin();
+//	auto p2 = s.end();
+
+//	auto p1 = std::make_reverse_iterator(s.end());
+//	auto p2 = std::make_reverse_iterator(s.begin());
+
+	auto p1 = s.rbegin(); // std::make_reverse_iterator(s.end()) 와동일
+	auto p2 = s.rend();
 
 
 	// 실행해 본후에.. 아래 코드는 변경하지 말고, 거꾸로 출력 되도록
