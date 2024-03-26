e989098037e95c88aca8207d74f5c32ee74d762d
ecourse <smkang@codenuri.co.kr>
2024-03-26 15:39:02 +0900

    Add files via upload

--- DAY2/9_단위전략1-1.cpp
+++ DAY2/9_단위전략1-1.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <mutex>
 
 template<typename T, typename ThreadModel > class List
 {	
@@ -10,6 +12,22 @@ public:
 		tm.unlock();
 	}
 };
+// 동기화 정책 클래스
+// => 규칙은 반드시 lock/unlock 이 있어야 한다.
+class NoLock
+{
+public:
+	inline void lock() {}
+	inline void unlock() {}
+};
+class MutexLock 
+{
+	std::mutex m;
+public:
+	inline void lock()   { m.lock(); std::cout << "lock\n";}
+	inline void unlock() { m.unlock(); std::cout << "unlock\n";}
+};
+
 
 
 
