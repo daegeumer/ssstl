8199c9261e0fd32d7dbc5d6becd792576af75bcb
ecourse <smkang@codenuri.co.kr>
2024-03-26 10:07:26 +0900

    Add files via upload

---
+++ DAY2/3_algo4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include "show.h"
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10};
+	std::list<int>   s = { 1,2,3,4,5,6,7,8,9,10 };
+
+	// 컨테이너를 뒤집을때 사용하는 함수 이름은 "reverse" 입니다
+	// v와 s 를 뒤집어 보세요
+
+	show(v);
+	show(s);	
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
