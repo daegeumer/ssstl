be680d021e65c1c666b0478ce8826d28aba6a499
ecourse <smkang@codenuri.co.kr>
2024-03-27 08:41:52 +0900

    Add files via upload

---
+++ DAY3/1_컨테이너의공통의특징.cpp
@@ -0,0 +1,40 @@
+﻿// STL 컨테이너의 특징
+#include <vector>
+#include <iostream>
+#include "show.h"
+
+template<typename T, typename Ax = std::allocator<T> >
+{
+	Ax ax;
+public:
+	// STL 컨테이너에는 많은 멤버 타입이 있습니다.
+	using iterator = vector_iterator;
+	using value_type = T;
+	using pointer = T*;
+	using size_type = std::size_t;
+
+	size_type size() const { return sz;}
+};
+
+
+int main()
+{
+	std::vector<int> v = { 1,2,3 };
+
+	// 1. 멤버 타입이 있다.
+	// => ? 자리에 최선의 코드를 적어 보세요(단, auto 사용하지 말고)
+
+	// => vector 의 크기를 담는 타입은 vector 설계자가 정확히 알고 있다.
+	//    vector 안에 멤버 타입으로 외부에 노출
+
+	std::vector<int>::size_type sz = v.size();
+
+    ? cnt = v.end() - v.begin();
+
+	// 2. 반환과 제거를 동시에 하지 않는다.
+	int n = v.pop_back();	// ?
+
+
+	// 3. 단위 전략을 많이 사용한다.
+	std::vector<int, debug_alloc<int>> v2; // ?
+}
