d81ddeae98e0adf3cbaf9e91cc59fb12ccb1e460
ecourse <smkang@codenuri.co.kr>
2024-03-26 16:56:10 +0900

    Add files via upload

--- DAY2/10_선형컨테이너3-2.cpp
+++ DAY2/10_선형컨테이너3-2.cpp
@@ -7,19 +7,19 @@
 
 int main()
 {
-	std::vector<int> v1;
-	std::vector<int> v2(1000);
-	
+	std::vector<int> v1;		// size = 0, capacity = 0
+	std::vector<int> v2(1000);  // size = 1000, capacity = 1000
+
 	std::vector<int> v3;
-	v3.reserve(1000);
+	v3.reserve(1000);		// size = 0, capacity = 1000
 
-	v1.push_back(0);
-	v2.push_back(0);
-	v3.push_back(0);
+	v1.push_back(0);	// 메모리 1개 할당해서 1번째 넣기
+	v2.push_back(0);	// 메모리 2000개할당헤서 1001번째 넣기
+	v3.push_back(0);	// 메모리 재할당 없고, 1번째 넣기
 
-
-
-//	std::cout << v.size() << ", " << v.capacity() << std::endl; // 10, 10
+	std::cout << v1.size() << ", " << v1.capacity() << std::endl; 
+	std::cout << v2.size() << ", " << v2.capacity() << std::endl; 
+	std::cout << v3.size() << ", " << v3.capacity() << std::endl; 
 }
 
 
