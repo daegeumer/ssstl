979310ac5d5e78494aa37740629e4ea16221e77a
ecourse <smkang@codenuri.co.kr>
2024-03-27 16:43:28 +0900

    Add files via upload

--- DAY3/10_사용자정의타입2.cpp
+++ DAY3/10_사용자정의타입2.cpp
@@ -27,9 +27,24 @@ int main()
 	std::vector<Point> v;
 
 	// 실행결과 예측해 보세요
-	// 1. 객체를 생성후 넣기.
-	Point pt(1, 2);
-	v.push_back(pt); 
+	// #1. 객체를 생성후 넣기.
+//	Point pt(1, 2);
+//	v.push_back(pt);
+
+	// #2. 임시객체 형태로 넣기
+//	v.push_back( Point(1, 2));
+
+	// #3. 임시객체를 만드는 또다른 표기법
+//	v.push_back( {1, 2} ); // C++11 부터 가능한 표기법
+
+	// #4. 객체를 전달하지 말고,
+	//     객체를 만들기 위한 생성자 인자만 전달하자.
+
+	v.emplace_back(1, 2); // vector 가 내부적으로
+						  // 자신의 버퍼에
+						  // "new Point(1,2)"
+
+
 
 	std::cout << "------------------" << std::endl;	
 }
