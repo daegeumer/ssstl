d76b1c4b39df526e598cd59878477fd1ef921f9f
ecourse <smkang@codenuri.co.kr>
2024-03-27 16:09:41 +0900

    Add files via upload

---
+++ DAY3/9_unordered_map2.cpp
@@ -0,0 +1,42 @@
+﻿// 7_map2 예제 복사해오세요
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include "show.h"
+
+int main()
+{
+	std::ifstream fin("7_map2.cpp");
+
+	std::string s;
+	int no = 0;
+	std::string word;
+
+//	std::map<std::string, std::vector<int>> index; 
+	std::unordered_map<std::string, std::vector<int>> index; 
+
+	while( std::getline(fin, s) )
+	{
+		++no;
+		std::istringstream iss(s);
+
+		while( iss >> word )
+		{
+			index[word].push_back(no);
+		}
+	}
+	//==========================
+	for( const auto& [key, value] : index) 
+	{
+		std::cout << key << " : ";
+		show(value);
+	}
+
+}
+
+
