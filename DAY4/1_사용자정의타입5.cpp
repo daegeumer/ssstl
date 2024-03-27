11ad1a37edfeffc96aafae95093a9620689f31b5
ecourse <smkang@codenuri.co.kr>
2024-03-28 08:38:37 +0900

    Add files via upload

---
+++ DAY4/1_사용자정의타입5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <set>
+#include "show.h"
+
+struct People
+{
+	std::string name;
+	int  age;
+
+	People(const std::string& n, int a) : name(n), age(a) {}
+};
+
+int main()
+{
+	// #1. std::set 에 보관되는 타입은 반드시 크기 비교가 가능해야 합니다.
+
+//	std::set<People> s; // error. 이 코드는 결국 아래 코드 입니다
+//  std::set<People, std::less<People>, std::allocator<People>> s;	
+//  => 즉, 요소의 비교를 위해서 "std::less<People>" 을 사용한다는 의미
+//  => std::less 는 "<" 로 비교 하고 있습니다.
+//  => 해결책은 "비교함수객체" 를 변경
+
+	std::set<People, PeopleCompare> s;
+	
+
+	s.emplace("kim", 20); // s.insert( People("kim",20))
+	s.emplace("lee", 25);
+	s.emplace("park", 30);
+
+	People peo("lee", 25);
+	auto p = s.find( peo );
+
+	std::cout << (*p).name << std::endl;
+}
+
