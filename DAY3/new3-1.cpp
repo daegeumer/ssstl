e8571cdfbf19980f5fe8ea03de6ac2a2065101a1
ecourse <smkang@codenuri.co.kr>
2024-03-27 13:58:28 +0900

    Add files via upload

--- DAY3/new3-1.cpp
+++ DAY3/new3-1.cpp
@@ -10,15 +10,17 @@ public:
 
 int main()
 {
-	std::vector<Point> v = {{1,1},{2,2},{3,3}};
+	std::vector<Point> v(5, Point(0,0)); // 5개를 Point(0,0) 으로
 	
 	std::cout << "--------------\n";
 
-	v.resize(2);	// 메모리 제거 안됨
+	v.resize(2, Point(0,0));	// 메모리 제거 안됨
+					// 하지만 줄어든 3개의 객체에 대해서 소멸자 호출
 
 	std::cout << "--------------\n";
 	
-	v.push_back(Point{4,4});
+	v.push_back(Point{4,4});	// 메모리 이미 있음.
+								// 하지만 해당 메모리에 대해 생성자를 호출
 
 	std::cout << "--------------\n";
 }
