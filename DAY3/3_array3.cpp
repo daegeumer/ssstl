3bd19d2cb8c40349b4fe650f6fdedfb96ea38437
ecourse <smkang@codenuri.co.kr>
2024-03-27 10:07:18 +0900

    Add files via upload

--- DAY3/3_array3.cpp
+++ DAY3/3_array3.cpp
@@ -7,6 +7,8 @@ int main()
 
 	std::cout << arr.size() << std::endl;
 
-	arr.push_back(0);
-	arr.resize(20);
+	// std::array 는 결국 "raw array" 입니다.
+	// "raw array 로 할수 없는 작업" 은 std::array 로도 안됩니다.
+	arr.push_back(0); // error
+	arr.resize(20);	  // error
 }
