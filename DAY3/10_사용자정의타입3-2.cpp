02b45692127fb3614c5aea36ec00ce5f64c11688
ecourse <smkang@codenuri.co.kr>
2024-03-27 17:07:06 +0900

    Add files via upload

---
+++ DAY3/10_사용자정의타입3-2.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <algorithm>
+#include "show.h"
+
+
+class Point
+{
+public:
+	int x, y;
+
+	Point(int a = 0, int b = 0) : x(a), y(b)
+	{
+		std::cout << "Point()" << std::endl;
+	}
+	~Point() { std::cout << "~Point()" << std::endl; }
+
+	Point(const Point& p)
+	{
+		std::cout << "Point(const Point&)" << std::endl;
+	}
+	void dump() const 
+	{
+		std::cout << x << ", " << y << std::endl;
+	}
+
+	// 컴파일러에게 <=> 연산자 구현을 요청
+	auto operator<=>(const Point& other) const = default;
+};
+
+
+
+int main()
+{
+	// C++20 의 새로운 연산자 ( three way comparison, "우주선연산자" 라는 별명)
+	auto ret = 1 <=> 3; // a <=> b
+						// a가 크면 ret > 0
+						// b가 크면 ret < 0
+						// a==b    ret == 0
+
+
+	Point p1(1,2);
+	Point p2(2,3);
+
+//	bool b = p1 > p2; // ok
+
+
+
+}
+
+
+
+
