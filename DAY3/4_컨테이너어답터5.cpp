2e098c8231310b6ceb033e449764bc7ff4b491ef
ecourse <smkang@codenuri.co.kr>
2024-03-27 11:11:35 +0900

    Add files via upload

--- DAY3/4_컨테이너어답터5.cpp
+++ DAY3/4_컨테이너어답터5.cpp
@@ -1,7 +1,26 @@
 #include "show.h"
 #include <queue> // queue 와 priority_queue
 
+template<typename T>
+class PQ : public std::priority_queue<T>
+{
+public:
+	void print() const 
+	{
+		show(c); // 기반 클래스인 std::priority_queue<T> 가 가진 
+				 // 컨테이너의 멤버이름이 "c" 입니다.
+	}
+};
+
 int main()
 {
-	
+	PQ<int> pq;
+
+	pq.push(10); 
+	pq.push(-10);
+	pq.push(-20);
+	pq.push(20);
+	pq.push(15);
+	pq.print();
+
 }
