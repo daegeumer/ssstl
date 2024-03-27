ff66657192dd077488e6a129deb0d5ac23dd31e1
ecourse <smkang@codenuri.co.kr>
2024-03-27 11:43:04 +0900

    Add files via upload

---
+++ DAY3/5_유틸리티2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream> // 표준 입출력 스트림
+#include <fstream>  // 화일 스트림
+#include <sstream>  // 메모리(스트링)스트림
+#include <string>
+
+// C++에는 3개의 스트림이 있습니다
+namespace std
+{
+	class ostream {};
+	class ofstream {};
+	class ostring_stream() {};
+
+	ostream cout; // 미리 객체가 생성되어 있습니다.
+}
+
+int main()
+{
+	std::cout << "hello";
+
+}
