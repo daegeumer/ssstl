a22c430dbf96d380b48a6f02c0da4506d4f9ef8b
ecourse <smkang@codenuri.co.kr>
2024-03-27 10:28:54 +0900

    Add files via upload

---
+++ DAY3/4_컨테이너어답터1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+// list 가 이미 있습니다
+#include <list>
+#include <vector>
+#include <deque>
+
+// 그런데, 스택이 필요합니다.
+// 1. 새로 만들자.
+// 2. list를 스택처럼 보이게 하자.
+
+// 1. list 의 멤버 함수이름을 stack 처럼 보이게 하고
+// 2. list 를 뒷쪽으로만 입/출력 할수 있게 하자
+
+template<typename T>
+class stack 
+{
+	std::list<T> c;
+public:
+	void push(const T& a) { c.push_back(a);}
+	void pop()            { c.pop_back();}
+	T&   top()            { return c.back();}
+};
+
+
+int main()
+{
+	stack<int> s;
+	s.push(10);
+	s.push(20);
+
+	std::cout << s.top() << std::endl;
+
+}
