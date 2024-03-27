2d85f45a8a26c8df26f54c4eb7236c55e45d9685
ecourse <smkang@codenuri.co.kr>
2024-03-28 08:49:29 +0900

    Add files via upload

---
+++ DAY4/1_사용자정의타입6.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include "show.h"
+
+struct People
+{
+	std::string name;
+	int  age;
+
+	People(const std::string& n, int a) : name(n), age(a) {}
+};
+
+struct PeopleHash
+{
+	int operator()(const People& p) const 
+	{
+
+	}
+};
+
+
+
+
+
+
+int main()
+{
+	/*
+	std::unordered_set< int, 
+						std::hash<int>, 
+						std::equal_to<int>, 
+						std::allocator<int>>
+
+//	std::unordered_set<People> s; // 이 코드는 아래와 같습니다.
+	std::unordered_set<People, std::hash<People>, std::equal_to<People>, std::allocator<People> > s; 
+*/
+	std::unordered_set<People, PeopleHash, PeopleEqual > s
+
+
+	s.emplace("kim", 20); // s.insert( People("kim",20))
+	s.emplace("lee", 25);
+	s.emplace("park", 30);
+
+	People peo("lee", 25);
+	auto p = s.find( peo );
+
+	std::cout << (*p).name << std::endl;
+}
+
