a6a91731b6d657c394188b6f518e3275d0d6ac42
ecourse <smkang@codenuri.co.kr>
2024-03-27 13:40:42 +0900

    Add files via upload

--- DAY3/new2.cpp
+++ DAY3/new2.cpp
@@ -10,6 +10,10 @@ public:
 
 int main()
 {
-	Point* p1 = new Point(1,2);
-	delete p1;
+//	Point* p1 = new Point(1,2); // 메모리 할당 + 생성자 호출
+//	delete p1;					// 소멸자 호출 + 메모리 해지
+
+	Point* p2 = static_cast<Point*>(operator new(sizeof(Point))); // 메모리 할당. malloc
+
+	operator delete(p2); // 메모리만 해지. free
 }
