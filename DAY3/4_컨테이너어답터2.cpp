1428ff87a062206d17fc74e3d2fb189297e6d49f
ecourse <smkang@codenuri.co.kr>
2024-03-27 10:45:37 +0900

    Add files via upload

---
+++ DAY3/4_컨테이너어답터2.cpp
@@ -0,0 +1,19 @@
+﻿// 3개의 컨테이너 어답터가 있습니다.
+#include <stack>
+#include <queue> // queue, priority_queue
+#include "show.h"
+
+int main()
+{
+	std::stack<int> s1;
+	s1.push(10);
+//	s1.pop(); // 제거
+	int n = s1.top();
+
+	std::queue<int> q1;
+	q1.push(10);
+//	q1.pop();
+	int n2 = q1.front(); // <== !!
+
+
+}
