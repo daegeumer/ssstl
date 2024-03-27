29c40e927dfdc48702ea3b744d4b9286e04be6a3
ecourse <smkang@codenuri.co.kr>
2024-03-27 10:39:19 +0900

    Add files via upload

---
+++ DAY3/4_컨테이너어답터1-2.cpp
@@ -0,0 +1,11 @@
+#include "show.h"
+#include <stack>
+
+int main()
+{
+	std::stack<int> s;
+
+	s.push(0); // 이순간 메모리 할당 정보를 볼수 있도록
+				// 위 코드에 allocator 를 변경해 보세요, debug_alloc 으로
+				// 1-1.cpp 코드를 잘보고 생각해보세요
+}
