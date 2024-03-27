dad90bb6f9775d343c0a34534291ca19ffce63c0
ecourse <smkang@codenuri.co.kr>
2024-03-27 15:40:14 +0900

    Add files via upload

--- DAY3/7_map2.cpp
+++ DAY3/7_map2.cpp
@@ -10,10 +10,30 @@
 int main()
 {
 	std::ifstream fin("7_map2.cpp");
+
 	std::string s;
 	int no = 0;
+	std::string word;
 
 	std::map<std::string, std::vector<int>> index; // 핵심. 자료구조생각해보세요
+
+	while( std::getline(fin, s) )
+	{
+		++no;
+		std::istringstream iss(s);
+
+		while( iss >> word )
+		{
+			index[word].push_back(no);
+		}
+	}
+	//==========================
+	for( const auto& [key, value] : index) 
+	{
+		std::cout << key << " : ";
+		show(value);
+	}
+
 }
 
 
