701b9a2d4feca59314b7f997eaacc92bbcdc34ac
ecourse <smkang@codenuri.co.kr>
2024-03-27 16:58:04 +0900

    Add files via upload

--- DAY3/10_사용자정의타입3-1.cpp
+++ DAY3/10_사용자정의타입3-1.cpp
@@ -1,4 +1,4 @@
-﻿
+
 #include <vector>
 #include <algorithm>
 #include "show.h"
@@ -21,9 +21,20 @@ public:
 	{
 		std::cout << x << ", " << y << std::endl;
 	}
+	// 사용자 정의 타입의 크기 비교를 위해서는 < 와 == 를 만드세요
+	bool operator<(const Point& other) const {	return x < other.x;	}
+	bool operator==(const Point& other) const {	return x == other.x && y == other.y;}
 };
+using namespace std::rel_ops; // !=, <=, >=, > 의 template 버전 제공
+
 int main()
 {
+	Point p1(1,2);
+	Point p2(2,3);
+
+	bool b = p1 > p2; // ok
+	
+
 	std::vector<Point> v;
 
 	v.emplace_back(1, 2); // Point(1,2)
@@ -31,17 +42,8 @@ int main()
 	v.emplace_back(3, 2);
 	v.emplace_back(0, 1);
 
-	// 사용자 정의 타입을 컨테이너 보관할수 있지만
-	// 알고리즘 사용시에는 알고리즘의 요구조건을 충족해야 합니다.
-	// => std::sort() 사용하려면 크기 비교가 가능해야 합니다.
-//	std::sort(v.begin(), v.end()); // error
+	std::sort(v.begin(), v.end()); 
 
-	// 방법 1. 람다표현식으로 비교 정책전달
-	std::sort( v.begin(), v.end(), 
-				[](const Point& p1, const Point& p2){ return p1.x < p2.x;});
-
-	// 방법 2. Point 타입 자체에 < 연산자를 제공하면 됩니다.
-	// => 다음 예제(3-1.cpp)
 }
 
 
