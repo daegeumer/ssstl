a5a2708e4801237a44af3485f850daeb789554f6
ecourse <smkang@codenuri.co.kr>
2024-03-27 11:47:33 +0900

    Add files via upload

--- DAY3/5_유틸리티2.cpp
+++ DAY3/5_유틸리티2.cpp
@@ -4,17 +4,27 @@
 #include <string>
 
 // C++에는 3개의 스트림이 있습니다
+/*
 namespace std
 {
-	class ostream {};
-	class ofstream {};
-	class ostring_stream() {};
+	class ostream {};	// 표준 출력 스트림
+	class ofstream {};  // 화일 출력
+	class ostringstream() {}; // 메모리(string)에 출력
 
 	ostream cout; // 미리 객체가 생성되어 있습니다.
 }
-
+*/
 int main()
 {
-	std::cout << "hello";
+	std::ofstream fout("a.txt");
+	std::ostringstream oss;
 
+	std::cout << "hello";
+	     fout << "hello";	// a.txt에 출력
+		 oss  << "hello";   // oss 가 내부적으로 가진 버퍼(string)에 출력
+
+	// oss 가 가진 문자열 꺼내기
+	std::string s = oss.str();
+
+	std::cout << s << std::endl; // hello
 }
