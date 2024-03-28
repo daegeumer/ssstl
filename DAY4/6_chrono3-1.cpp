d677b9b1c5cb0d54bf8e1d79cbcef42163b0f9ad
ecourse <smkang@codenuri.co.kr>
2024-03-28 13:38:18 +0900

    Add files via upload

---
+++ DAY4/6_chrono3-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std::literals;
+
+int main()
+{
+	std::cout << "======\n";
+
+	// 현재스레드를 특정시간동안 재우는 함수는  "std::this_thread::sleep_for"
+	// 입니다.
+	// 3초간 재워 보세요.
+
+	
+
+	std::cout << "======\n";
+}
