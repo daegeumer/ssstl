f97dd027f3640d0ee991e657d5a4320bae85522e
ecourse <smkang@codenuri.co.kr>
2024-03-28 10:42:49 +0900

    Add files via upload

---
+++ DAY4/3_bind_function1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <functional>
+using namespace std::placeholders;
+
+void f1(int a)               { printf("f1 : %d\n", a); }
+void f2(int a, int b)        { printf("f2 : %d, %d\n", a, b);}
+
+int main()
+{
+	// C언어의 함수 포인터는 유연성이 부족합니다.
+    void(*f)(int) = &f1; // ok
+	f = &f2; 			 // error    
+						// f 에는 인자가 한개인 함수의 주소만 담을수 있습니다.
+						// f 에는 멤버 함수 주소도 담을수 없습니다.
+}
+
+
