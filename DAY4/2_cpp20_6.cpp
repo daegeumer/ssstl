b486eea2c0253c57973e883f74d93240ef50915a
ecourse <smkang@codenuri.co.kr>
2024-03-28 10:15:21 +0900

    Add files via upload

--- DAY4/2_cpp20_6.cpp
+++ DAY4/2_cpp20_6.cpp
@@ -13,16 +13,28 @@ class take_view
 public:
 	take_view(R& r, std::size_t c) : rg(r), cnt(c) {}
 
-	auto begin() { return r.begin();}
-	auto end()   { return std::next(r.begin(), cnt);}
+	auto begin() { return rg.begin();}
+	auto end()   { return std::next(rg.begin(), cnt);}
 };
+
+template<typename R>
+class reverse_view
+{
+	R& rg;
+public:
+	reverse_view(R& r) : rg(r) {}
+
+	auto begin() { return rg.rbegin();}
+	auto end()   { return rg.rend();}
+};
+
 int main()
 {
 	std::vector<int> v = {1,2,3,4,5,6,7,8,9,10};
 	take_view tv(v, 5);
-	
 //	for(auto e : v)
-	for(auto e : tv)
+//	for(auto e : tv)
+	for(auto e : reverse_view(v) )
 	{
 		std::cout << e << ", ";
 	}	
