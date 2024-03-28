9c1aa56edc4f93e0aa4f1dc5dfb3bea56d4cfaf6
ecourse <smkang@codenuri.co.kr>
2024-03-28 16:00:36 +0900

    Add files via upload

--- DAY4/4_스마트포인터10.cpp
+++ DAY4/4_스마트포인터10.cpp
@@ -34,7 +34,20 @@ int main()
 	}
 	else
 	{
-		
+		// 파괴되지 않은 경우 객체에 접근하려면
+//		auto name = sp1->bf->name; // error
+									// weak_ptr 은 객체에 접근 할수없음.
+
+		// weak_ptr 을 사용해서 shared_ptr 을 다시 한개 생성후 접근해야 합니다.
+
+		std::shared_ptr<People> sp = sp1->bf.lock();
+
+		// sp 를 다시 확인후 사용해야 합니다.
+		if ( sp )
+		{
+			// sp 사용...
+			auto name = sp->bf->name;
+		}
 	}
 }
 
