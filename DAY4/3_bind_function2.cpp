13abdf29b1bc770ed2b0f8ff09f93d9a70ec33b8
ecourse <smkang@codenuri.co.kr>
2024-03-28 10:45:38 +0900

    Add files via upload

---
+++ DAY4/3_bind_function2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <functional>
+using namespace std::placeholders;
+
+// 4항 함수
+void foo( int a, int b, int c, int d)
+{
+    printf("%d, %d, %d, %d\n", a, b, c, d);
+}
+
+int main()
+{
+    foo(1,2,3,4);   // 4항 함수
+
+	// std::bind : 기존 함수(객체)의 특정 인자를 고정한 새로운 함수 만드는 도구
+	//				M항 함수 => N 항 함수로 만들기( 단, M >= N)
+	
+	// 사용법 : std::bind(&M항함수, M개 인자)
+	
+	auto f1 = std::bind(&foo, 1, 2, _1, _2);
+}
+
