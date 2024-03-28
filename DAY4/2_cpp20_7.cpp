a50f1bf85bfac18477746e5d4f8c3963ec3a2e19
ecourse <smkang@codenuri.co.kr>
2024-03-28 10:21:14 +0900

    Add files via upload

---
+++ DAY4/2_cpp20_7.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <iostream>
+#include <ranges>
+#include "show.h"
+
+int main()
+{
+	std::vector<int> v = {1,2,3,4,5,6,7,8,9,10};
+
+	// #1. 중첩도 가능
+	std::ranges::take_view tv(v, 5);
+	std::ranges::reverse_view rv(tv);
+
+	// #2. 단축 표기법(syntax sugar) 도 제공합니다.
+	// => 리눅스 의 파이프라인은 흉내낸것
+
+	auto rv2 = v | std::views::filter( [](int n) {return n % 2 == 0;} ) 
+				 | std::views::take(3) | std::views::reverse;
+
+	for(auto e : rv2)
+	{
+		std::cout << e << ", ";
+	}	
+}
+
