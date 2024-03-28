f524549de64f8818c873c566c26abdd4d7e3617c
ecourse <smkang@codenuri.co.kr>
2024-03-28 16:48:07 +0900

    Add files via upload

---
+++ DAY4/5_unique_ptr2.cpp
@@ -0,0 +1,17 @@
+﻿#include <memory>
+#include <iostream>
+
+// std::shared_ptr : 자원을 공유할수 있는 스마트 포인터
+// std::weak_ptr   : 소유권이 없는 스마트 포인터
+// std::unique_ptr : 자원을 독점하는 스마트 포인터.
+
+int main()
+{
+	std::unique_ptr<int> up1(new int); // ok 
+//	std::unique_ptr<int> up2 = up1;	   // error
+	std::unique_ptr<int> up2 = std::move(up1); // ok
+
+	std::unique_ptr<int> up3 = std::make_unique<int>();
+
+	std::cout << sizeof(up1) << std::endl; // raw pointer 와 같은 크기
+}
---
+++ DAY4/5_unique_ptr3.cpp
@@ -0,0 +1,33 @@
+﻿#include <memory>
+#include <iostream>
+
+struct Freer 
+{
+	void operator()(void* p)
+	{
+		free(p);
+		std::cout << "Freer operator()\n";
+	}
+};
+
+int main()
+{
+	// 삭제자 변경
+	std::unique_ptr<int> up1(new int);      // ok
+
+//	std::unique_ptr<int> up2(new int[10]);  // 버그. 소멸자가 delete. 
+											// 하지만 delete[] 해야 한다.
+	
+	std::unique_ptr<int[]> up2(new int[10]);  // ok
+												// 단, C++14부터
+	//----------------------------------------------------
+	// 삭제자를 변경하려면
+	// => 템플릿 2번째 인자로 삭제 방법을 담은 함수 객체 전달
+	std::unique_ptr<int, Freer > up3(static_cast<int*>(malloc(sizeof(int)));
+
+
+	// shared_ptr vs unique_ptr
+	
+
+
+}
