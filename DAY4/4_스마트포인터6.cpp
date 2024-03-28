fb00146f70f48376da29a49aa1111f684063142c
ecourse <smkang@codenuri.co.kr>
2024-03-28 15:07:31 +0900

    Add files via upload

---
+++ DAY4/4_스마트포인터6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <memory>
+
+void foo(void* p) { free(p); }
+
+int main()
+{
+	// 삭제자 변경
+	std::shared_ptr<int> sp1(new int); // ok. sp1 의 소멸자가 delete
+
+
+	std::shared_ptr<int> sp2(static_cast<int*>(malloc(sizeof(int))));
+
+}
+
+
+
+
+
+
+
