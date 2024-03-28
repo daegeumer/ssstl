d6e8dea50d518809e5d8a8e3056bb82892442fe6
ecourse <smkang@codenuri.co.kr>
2024-03-28 10:11:34 +0900

    Add files via upload

--- DAY4/2_cpp20_6.cpp
+++ DAY4/2_cpp20_6.cpp
@@ -5,11 +5,24 @@
 #include <ranges>
 #include "show.h"
 
+template<typename R>
+class take_view
+{
+	R& rg;
+	std::size_t cnt;
+public:
+	take_view(R& r, std::size_t c) : rg(r), cnt(c) {}
+
+	auto begin() { return r.begin();}
+	auto end()   { return std::next(r.begin(), cnt);}
+};
 int main()
 {
 	std::vector<int> v = {1,2,3,4,5,6,7,8,9,10};
-
-	for(auto e : v)
+	take_view tv(v, 5);
+	
+//	for(auto e : v)
+	for(auto e : tv)
 	{
 		std::cout << e << ", ";
 	}	
