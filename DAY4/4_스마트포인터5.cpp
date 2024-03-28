893e5a301da2802aa7c050e23ff0c2740d46963b
ecourse <smkang@codenuri.co.kr>
2024-03-28 14:48:02 +0900

    Add files via upload

---
+++ DAY4/4_스마트포인터5.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <memory>
+
+
+void* operator new(size_t sz)
+{
+	std::cout << "new : " << sz << std::endl;
+	return malloc(sz);
+}
+
+struct Point
+{
+	int x, y;
+
+	Point(int a = 0, int b = 0) : x(a), y(b) {}
+};
+
+int main()
+{
+	// 아래 코드는 동적 메모리 할당을 몇번하게 될까요 ?
+	// => 2번의 메모리 할당
+	// => 1. new Point(1,2)
+	// => 2. shared_ptr 의 생성자에서 new control_block(참조계수등)
+	std::shared_ptr<Point> sp(new Point(1, 2));
+	
+	
+}
+
+
+
+
+
+
+
+
+void foo( std::shared_ptr<int> sp, int n) {}
+
+int goo() { return 10; }
+
