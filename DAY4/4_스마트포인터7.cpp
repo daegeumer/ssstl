5debd819e82a686ba75ec096dc76e42678f4b713
ecourse <smkang@codenuri.co.kr>
2024-03-28 15:42:50 +0900

    Add files via upload

---
+++ DAY4/4_스마트포인터7.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <iostream>
+#include <memory>
+
+struct People
+{
+	std::string name;
+
+	People(std::string n) : name(n) {}
+	~People() { std::cout << name << " 파괴" << std::endl; }
+
+	std::shared_ptr<People> bf;  // "best friend"
+};
+
+int main()
+{
+	std::shared_ptr<People> sp1(new People("kim"));
+	std::shared_ptr<People> sp2(new People("lee"));
+
+	// 스마트포인터를 아래처럼 사용하면 자원 누수가 발생합니다.
+	// => 상호 참조, 원형 참조라고 합니다.(cycle reference)
+	sp1->bf = sp2;
+	sp2->bf = sp1;
+}
+
+
