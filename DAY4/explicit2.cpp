8ad86a50ddd8b3334029411c6f135776b29e6a03
ecourse <smkang@codenuri.co.kr>
2024-03-28 17:15:06 +0900

    Add files via upload

--- DAY4/explicit2.cpp
+++ DAY4/explicit2.cpp
@@ -6,11 +6,13 @@ void f2(std::string s) {}
 
 int main()
 {
-	f2("hello");
-	std::string s1 = "hello";
-	std::string s2("hello");
+	f2("hello"); // ok
+	std::string s1 = "hello"; // ok
+	std::string s2("hello");  // ok 
+							// string(const char*) 생성자는 explicit 아님.
 
-	f1(10);
-	std::vector<int> v1 = 10;
-	std::vector<int> v2(10);
+	f1(10);	// error
+	std::vector<int> v1 = 10;	// error
+	std::vector<int> v2(10);	// ok
+								// vector(int) 생성자는 explicit!!
 }
