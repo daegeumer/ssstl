039383d930646b3542de03fe93ea625855a38d80
ecourse <smkang@codenuri.co.kr>
2024-03-28 11:21:46 +0900

    Add files via upload

--- DAY4/3_bind_function4.cpp
+++ DAY4/3_bind_function4.cpp
@@ -8,21 +8,37 @@ using namespace std::placeholders;
 
 class NotificationCenter
 {
+	using HANDLER = std::function<void(void*)>;
+
+	std::map<std::string, std::vector<HANDLER> > notif_map;
+
 public:
+	void addObserver( const std::string& key, HANDLER h)
+	{
+		notif_map[key].push_back(h);
+	}
+	void postNotificationWithName(const std::string& key, void* hint)
+	{
+		for( auto& f : notif_map[key] )
+		{
+			f(hint); // 현재 f는 std::function 
+		}
+	}
 };
+// 아래 main 에서 goo 등록시 에러 있습니다. 수정해 보세요
 
 void foo(void* p)        { std::cout << "foo : " << (int)p << std::endl; }
 void goo(void* p, int a) { std::cout << "goo : " << (int)p << std::endl; }
 
-
 int main()
 {
+	// IOS 의 cocoa touch 라이브러리에 있는 클래스를 C++로 만든것
 	NotificationCenter nc;
 
 	nc.addObserver("LOWBATTERY", &foo);
 	nc.addObserver("LOWBATTERY", &goo);
 
-	// 배터리 모듈쪽에서 배터리가 부족해지면
+	// 배터리 모듈쪽에서 배터리가 부족해지면 - OS내부에서 이렇게 하고 있다고 가정
 	nc.postNotificationWithName("LOWBATTERY", (void*)30);
 }
 
