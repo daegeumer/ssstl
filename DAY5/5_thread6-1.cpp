1df8da5593b0b4901d9056ceb6cecdca4c9f4359
ecourse <smkang@codenuri.co.kr>
2024-03-29 14:17:21 +0900

    Add files via upload

---
+++ DAY5/5_thread6-1.cpp
@@ -0,0 +1,17 @@
+#include <atomic>
+#include <iostream>
+
+struct Point 
+{
+	int x;
+	int y;
+};
+
+int main()
+{
+	std::atomic<Point> p1;
+	std::atomic<Point> p2(p1.load()); // 멀티스레드 안전하게 복사
+
+	std::cout << p1.lock_free() << std::endl;
+
+}
