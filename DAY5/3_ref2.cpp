cc87e1e4467452a9153e1928d76ae6e06ff8917e
ecourse <smkang@codenuri.co.kr>
2024-03-29 09:53:42 +0900

    Add files via upload

---
+++ DAY5/3_ref2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <functional>
+
+// reference_wrapper 의 원리를 생각해 봅시다.
+template<typename T>
+class reference_wrapper
+{
+
+};
+
+int main()
+{
+	int n1 = 10;
+	int n2 = 20;
+
+	reference_wrapper<int> r1 = n1;  
+	reference_wrapper<int> r2 = n2;
+
+	r1 = r2;	
+
+	std::cout << n1 << std::endl; 
+	std::cout << n2 << std::endl; 
+	std::cout << r1 << std::endl; 
+	std::cout << r2 << std::endl; 
+}
+
