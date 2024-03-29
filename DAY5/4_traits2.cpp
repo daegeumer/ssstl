b6dd43d0f7256c4cde8c1876e0ce0d5bdca51811
ecourse <smkang@codenuri.co.kr>
2024-03-29 10:51:59 +0900

    Add files via upload

--- DAY5/4_traits2.cpp
+++ DAY5/4_traits2.cpp
@@ -21,7 +21,9 @@ template<typename T> struct is_pointer<T*>
 template<typename T> void foo(const T& b)
 {
 	// T : int, int*
+
 	if ( is_pointer<T>::value )
+	
 		std::cout << "포인터" << std::endl;
 	else
 		std::cout << "포인터 아님" << std::endl;
---
+++ DAY5/4_traits3.cpp
@@ -0,0 +1,22 @@
+// 4_traits
+#include <iostream>
+#include <type_traits> // 이 안에 이미 "std::is_pointer" 있습니다.
+
+template<typename T> void printv(const T& v)
+{
+	if ( T 가 포인터 타입이라면 )
+		std::cout << v << " : " << *v << std::endl;
+	else 
+		std::cout << v << std::endl;
+}
+
+int main()
+{
+	int n = 10;
+	double d = 10;
+
+	printv(n);
+	printv(d);
+	printv(&n);
+}
+
