d176a092fbba6f4baacb942f2d9f2bd4b07f2597
ecourse <smkang@codenuri.co.kr>
2024-03-29 10:11:40 +0900

    Add files via upload

---
+++ DAY5/3_ref3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <functional>
+
+void foo(int& a) { a = 200; }
+
+// 2번째 인자를 다른 함수로 전달하고 있습니다.
+// => 그런데, 값의 개념으로 동작하도록 설계되었습니다.
+template<typename F, typename T> 
+void forward(F f, T arg)
+{
+	f(arg);
+}
+
+int main()
+{
+	int n = 10;
+//	foo(n);	// 직접 전달, n은 200으로 변경됩니다.
+
+//	forward(foo, n); // 복사본 생성됩니다.
+//	forward(foo, &n); // 주소로 전달.
+					  // T arg = &n 에서 T = int* 이므로
+					  // foo(int*) 가 되어서 에러
+
+	std::reference_wrapper<int> r = n;
+	forward(foo, r);
+
+	std::cout << n << std::endl; 
+}
