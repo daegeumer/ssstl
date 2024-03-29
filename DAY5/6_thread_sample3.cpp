f2ae4283fe2fff31d923b593a40b388f5b3a46e5
ecourse <smkang@codenuri.co.kr>
2024-03-29 15:18:33 +0900

    Add files via upload

--- DAY5/6_thread_sample3.cpp
+++ DAY5/6_thread_sample3.cpp
@@ -28,17 +28,16 @@ void sum(T first, T last, R& result)
 template<typename T, typename R>
 void parallel_sum(T first, T last, R& result)
 {
-	constexpr std::size_t cnt_per_thread = 25;
+	const std::size_t cnt_per_thread = 25;
 
-	constexpr std::size_t cnt_element = std::distance(first, last); // 요소갯수
+	const std::size_t cnt_element = std::distance(first, last); // 요소갯수
 	if ( cnt_element == 0) return;
 
 	// 스레드당 25개씩 분배할때 필요한 스레드의 갯수
-	constexpr std::size_t max_cnt_thread = 
+	const std::size_t max_cnt_thread = 
 				(cnt_element + cnt_per_thread-1) / cnt_per_thread;
 
-	// 현재 H/W 상 코어가 몇개 있는가
-	
+	// 현재 H/W 상 코어가 몇개 있는가	
 	std::size_t cnt_hw_thread = std::thread::hardware_concurrency();
 
 	if ( cnt_hw_thread == 0 )
@@ -48,13 +47,10 @@ void parallel_sum(T first, T last, R& result)
 }
 
 
-
-
-
 int main()
 {
     init();
-
     int s = 0;
-    sum(v.begin(), v.end(), s);
+//    sum(v.begin(), v.end(), s);
+    parallel_sum(v.begin(), v.end(), s);
 }
