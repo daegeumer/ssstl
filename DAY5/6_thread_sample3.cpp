98cb85524b9de4451d775e509c774f6d13baf9c0
ecourse <smkang@codenuri.co.kr>
2024-03-29 15:12:32 +0900

    Add files via upload

---
+++ DAY5/6_thread_sample3.cpp
@@ -0,0 +1,51 @@
+#include <thread>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <iostream>
+
+constexpr std::size_t sz = 1'000'000;
+
+std::vector<int> v;
+
+void init()
+{
+	v.reserve(sz);
+	
+    for (int i = 0; i < sz; ++i)
+    {
+        v.push_back(i);
+    }
+}
+//---------------------------------------------
+template<typename T, typename R>
+void sum(T first, T last, R& result)
+{
+    result = std::accumulate(first, last, result);
+}
+//------------------------------------------------------------
+template<typename T, typename R>
+void parallel_sum(T first, T last, R& result)
+{
+	constexpr std::size_t cnt_per_thread = 25;
+
+	constexpr std::size_t cnt_element = std::distance(first, last); // 요소갯수
+	if ( cnt_element == 0) return;
+
+	constexpr std::size_t max_cnt_thread = 
+				(cnt_element + cnt_per_thread-1) / cnt_per_thread;
+
+}
+
+
+
+
+
+int main()
+{
+    init();
+
+    int s = 0;
+    sum(v.begin(), v.end(), s);
+}
