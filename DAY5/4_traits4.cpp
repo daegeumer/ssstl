e4ddb1ffc9d04ed8685d77cc3a0593edd4f07f26
ecourse <smkang@codenuri.co.kr>
2024-03-29 11:08:02 +0900

    Add files via upload

---
+++ DAY5/4_traits4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <type_traits>
+
+// type traits 종류
+// 1. 타입 질의     : std::is_pointer<T>::value          ===> bool
+// 2. 변형 타입얻기 : std::remove_pointer<int*>::type    ===> int
+
+
+template<typename T> void foo(const T& b)
+{
+	typename std::remove_pointer<T>::type n;
+
+	std::cout << typeid(n).name() << std::endl;
+}
+
+int main()
+{
+	int n = 10;
+
+	printv(n);
+	printv(&n);
+}
+
