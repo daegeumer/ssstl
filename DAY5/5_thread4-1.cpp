24c7696d3489e393edea8cf4c9db9c255f32a4d6
ecourse <smkang@codenuri.co.kr>
2024-03-29 13:38:20 +0900

    Add files via upload

--- DAY5/5_thread4-1.cpp
+++ DAY5/5_thread4-1.cpp
@@ -5,10 +5,13 @@
 using namespace std::literals;
 
 // 방법 #1. out parameter 사용
-
+// => 단점 : 스레드가 종료 될때 까지 대기 해야 한다.(아래 주석 참고)
 void add(int a, int b, int& s)
 {
 	s = a + b;
+
+	// 자원정리등의 추가적인 작업
+	std::this_thread::sleep_for(2s);
 }
 int main()
 {
