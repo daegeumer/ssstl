4f0bf52ece56e8eec5e7a36b1361cfc0c7022372
ecourse <smkang@codenuri.co.kr>
2024-03-29 14:02:06 +0900

    Add files via upload

--- DAY5/5_thread5.cpp
+++ DAY5/5_thread5.cpp
@@ -8,6 +8,9 @@ using namespace std::literals;
 int add(int a, int b)
 {
 	int s = a + b;
+
+	std::cout << std::this_thread::get_id() << std::endl;
+
 	return s;
 }
 int main()
@@ -16,6 +19,10 @@ int main()
 	//				를 스레드로 수행하고 결과를 얻을때 사용
 
 	std::future<int> ft = std::async(&add, 1, 2);
+	std::future<int> ft1 = std::async(&add, 1, 2);
+	std::future<int> ft2 = std::async(&add, 1, 2);
+
+
 
 	int ret = ft.get();
 
@@ -23,3 +30,10 @@ int main()
 
 
 }
+
+// std::async    : 내부적으로 아래 2개를 사용해서 만들어진 편의 함수
+//			       => 단, 대부분의 컴파일러에서 구현시
+//				   => 스레드 풀링 기술로 구현해서, 성능도 좋습니다.
+
+// std::promise  : 스레드간 결과를 꺼내는 도구
+// std::thread 	 : C++에서 스레드를 생성하는 유일한 방법
