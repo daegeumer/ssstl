a3719f64bed72c3df9606734c6e4a0cbf3b30e04
ecourse <smkang@codenuri.co.kr>
2024-03-29 13:55:47 +0900

    Add files via upload

--- DAY5/5_thread5.cpp
+++ DAY5/5_thread5.cpp
@@ -1,1 +1,25 @@
-﻿// async - 4번 복사
+﻿#include <iostream>
+#include <thread>
+#include <future>
+#include <chrono>
+using namespace std::literals;
+
+// 스레드를 고려 하지 않은 함수
+int add(int a, int b)
+{
+	int s = a + b;
+	return s;
+}
+int main()
+{
+	// std::async : 스레드를 고려하지 않은 일반함수(결과를 반환값으로 알려주는)
+	//				를 스레드로 수행하고 결과를 얻을때 사용
+
+	std::future<int> ft = std::async(&add, 1, 2);
+
+	int ret = ft.get();
+
+	std::cout << ret << std::endl;
+
+
+}
