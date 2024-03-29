4476235609e4f9b41d3cb5babf85cff10499a828
ecourse <smkang@codenuri.co.kr>
2024-03-29 13:33:24 +0900

    Add files via upload

---
+++ DAY5/5_thread4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <thread>
+#include <future>
+#include <chrono>
+using namespace std::literals;
+
+// promise & future
+
+// 스레드를 고려 하지 않은 함수
+int add1(int a, int b)
+{
+	int s = a + b;
+	return s;
+}
+
+int main()
+{
+	// 핵심 1. 스레드가 아닌 직접 호출은 함수의 결과를 반환값으로 받으면 됩니다.
+	int ret1 = add1(1, 2);
+
+	// 스레드로 add1 수행하고, 반환값을 얻고 싶다.?
+	//=> 스레드로 수행한 함수의 반환값을 주스레드에서 받을수 없다.
+	std::thread t(&add1, 1, 2);
+
+	t.join();
+
+
+}
