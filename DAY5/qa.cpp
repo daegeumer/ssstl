76f175093f2ea22bf9b5c6b5a705705cf8ccfd04
ecourse <smkang@codenuri.co.kr>
2024-03-29 09:21:03 +0900

    Add files via upload

---
+++ DAY5/qa.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int x = 100;
+
+int main()
+{
+	const char* s1 = "ABCD";
+	const char* s2 = "XYZ";
+
+	printf("ABCD");
+}
+
+// 1. 빌드해서 실행파일 만드세요
+// 2. 구글에서 "PEView" 검색해서 PEView0.99 다운 받으세요
+// 3. PEView 실행해서 실행파일 여세요
