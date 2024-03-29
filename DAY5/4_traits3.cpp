d80d16179818bf6421545e0dbaac1bf7b975a525
ecourse <smkang@codenuri.co.kr>
2024-03-29 10:53:15 +0900

    Add files via upload

--- DAY5/4_traits3.cpp
+++ DAY5/4_traits3.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include <type_traits> // 이 안에 이미 "std::is_pointer" 있습니다.
 
-template<typename T> void printv(const T& v)
+template<typename T> 
+void printv(const T& v)
 {
-	if ( T 가 포인터 타입이라면 )
+	if ( std::is_pointer<T>::value  )
 		std::cout << v << " : " << *v << std::endl;
 	else 
 		std::cout << v << std::endl;
